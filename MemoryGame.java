91df54a253f224b2af650b4711d4b3993b043b29
therollingjon <therollingjon@gmail.com>
2017-04-12 23:59:00 +0300

    Add files via upload

---
+++ MemoryGame.java
@@ -0,0 +1,154 @@
+package memory_game;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class MemoryGame
+{
+	// Game has 4 rows and 4 columns:
+	static int[][] cards = new int[4][4];
+	static boolean upDown[][] = new boolean[4][4];
+	static Scanner s = new Scanner(System.in);
+
+	public static void main(String[] args)
+	{
+		showInitialWelcomeMessage();
+		setupBoard();
+		game(upDown, cards); // calls the game
+	}
+
+	// Here we salut the player and give the basic instructions about how to play.
+	public static void showInitialWelcomeMessage()
+	{
+		System.out.println("\nWELCOME!\n\nGive coordinates in an adjacent style like\nCo-ordinate 1: 12[row][column]\nCo-ordinate 2: 25[row][column]\n\n");
+	}
+	
+	// Here we are prompting the board to the user:
+	public static void setupBoard()
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			for (int a = 0; a < 4; a++)
+			{
+				upDown[i][a] = false;
+			}
+		}
+		cards = randomizer(); // Shuffle cards
+	}
+
+	// print the board
+	public static void displayBoard(boolean[][] upDown, int[][] cards)
+	{
+		System.out.println("     1 2 3 4 ");
+		System.out.println("---+---------");
+		for (int i = 0; i < 4; i++)
+		{
+			System.out.print(" " + (i + 1) + " | ");
+			for (int a = 0; a < 4; a++)
+			{
+				if (upDown[i][a])
+				{
+					System.out.print(cards[i][a]);
+					System.out.print(" ");
+				} else
+					System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	public static int[][] randomizer()
+	{
+		int num[] =
+			{ 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8 };
+		int cards[][] = new int[4][4];
+		Random random = new Random();
+		int temp, t;
+		for (int j = 0; j <= 20; j++)
+		{
+			for (int x = 0; x < 16; x++)
+			{ // Randomize the card order
+				t = random.nextInt(1000) % 15;
+				temp = num[x];
+				num[x] = num[t];
+				num[t] = temp;
+
+			}
+			t = 0;
+			for (int r = 0; r < 4; r++) // Cards receive Numbers
+			{
+				for (int s = 0; s < 4; s++)
+				{
+					cards[r][s] = num[t];
+					t = t + 1;
+				}
+			}
+
+		}
+		return cards;
+	}
+
+	// Start the Game
+	public static void game(boolean[][] upDown, int[][] cards)
+	{
+		int noDownCards = 16;
+		while (noDownCards > 0)
+		{
+			displayBoard(upDown, cards);
+			System.out.println("Enter Co-oridinate 1");
+			String g1 = s.next();
+			int g1x = Integer.valueOf(g1.substring(0, 1)) - 1;
+			int g1y = Integer.valueOf(g1.substring(1, 2)) - 1;
+			System.out.println("You've found " + cards[g1x][g1y] + " in given co-ordinates.");
+
+			System.out.println("\nEnter Co-oridinate 2");
+			String g2 = s.next();
+			int g2x = Integer.valueOf(g2.substring(0, 1)) - 1;
+			int g2y = Integer.valueOf(g2.substring(1, 2)) - 1;
+			System.out.println("You've found " + cards[g2x][g2y] + " in given co-ordinates.\n");
+			if (cards[g1x][g1y] == cards[g2x][g2y])
+			{
+				System.out.println("Bravo! You found a match!");
+				upDown[g1x][g1y] = true;
+				upDown[g2x][g2y] = true;
+				noDownCards -= 2;
+			}
+		}
+		displayBoard(upDown, cards);
+		System.out.println("You Win!");
+	}
+
+	// shuffle the cards
+	public static int[][] shuffleTheNumbers()
+	{
+		int start[] =
+			{ 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8 };
+		int cards[][] = new int[4][4];
+		Random ran = new Random();
+		int tmp, i;
+		for (int s = 0; s <= 20; s++)
+		{
+			for (int x = 0; x < 16; x++) // randomize the card placements
+			{
+				i = ran.nextInt(100000) % 15;
+				tmp = start[x];
+				start[x] = start[i];
+				start[i] = tmp;
+			}
+		}
+		i = 0;
+
+		for (int r = 0; r < 4; r++) // put values in cards here
+		{
+			for (int c = 0; c < 4; c++)
+			{
+				cards[r][c] = start[i];
+				i = i + 1;
+			}
+		}
+		return cards;
+
+	}
+
+}
